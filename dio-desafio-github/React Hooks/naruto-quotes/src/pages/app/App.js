79d1d98c27b83a34667a905decd8c1c2bd43b42f
nathanmintegui <nathanberger.rs@hotmail.com>
2022-06-23 00:21:28 -0300

    final project settings

--- dio-desafio-github/React Hooks/naruto-quotes/src/pages/app/App.js
+++ dio-desafio-github/React Hooks/naruto-quotes/src/pages/app/App.js
@@ -1,11 +1,38 @@
+import { useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import narutoImg from '../../images/naruto.png';
 import { Quotes } from '../../components';
+import { getQuote } from '../../services/quotesService';
+import jutsoSound from '../../sounds/src_sounds_jutso.mp3';
+
+
+const audio = new Audio(jutsoSound);
 
 export function App() {
+  const isMounted = useRef(true);
+
+  const [quoteState, setQuoteState] = useState({ quote: 'loading quote...', speaker: 'loading speaker...'});
+
+  const onUpdate = async () => {
+    const quote = await getQuote();
+
+    if(isMounted.current) {
+      audio.play();
+      setQuoteState(quote);
+    }
+  };
+
+  useEffect(() => {
+    onUpdate();
+
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
+
   return (
     <Content>
-      <Quotes quote={'ok'} speaker={'Speaker'} />
+      <Quotes quote={quoteState.quote} speaker={quoteState.speaker}  onUpdate={onUpdate} />
       <NarutoImg src={narutoImg} alt='Naruto with a Kunai' />
     </Content>
   );
--- dio-desafio-github/React Hooks/naruto-quotes/src/services/quotesService/quotesService.js
+++ dio-desafio-github/React Hooks/naruto-quotes/src/services/quotesService/quotesService.js
@@ -1,2 +1,2 @@
-export const QuotesService = () => 
+export const getQuote = () => 
     fetch(process.env.REACT_APP_API).then((response) => response.json());
